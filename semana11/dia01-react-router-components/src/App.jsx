4117d93d86c1ef662d80c8eb1d487a217b710456
Frank Parra <panf9188@gmail.com>
2023-08-21 13:49:36 -0500

    Updating

--- semana11/dia01-react-router-components/src/App.jsx
+++ semana11/dia01-react-router-components/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -8,6 +8,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
+        <nav>
+          <Link href="/"></Link>
+          <Link href="/login"></Link>
+          <Link href="/prueba"></Link>
+        </nav>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
